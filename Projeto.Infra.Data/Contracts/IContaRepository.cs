db560068db47b44dfd8e9f18074544b40dda06ca
DiegoSpinelli <diego.spinellids@gmail.com>
2020-09-19 12:36:24 -0300

    Projeto Dashboard - v1.0

---
+++ Projeto.Infra.Data/Contracts/IContaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Projeto.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infra.Data.Contracts
+{
+    public interface IContaRepository : IBaseRepository<Conta>
+    {
+        List<Conta> GetByDatas(DateTime dataMin, DateTime dataMax);
+        decimal GetTotalPorTipo(TipoConta tipoConta);
+    }
+}
+
